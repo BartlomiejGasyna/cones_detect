718a812f46e7cc851809f93550ab1c1e57bedf20
Bartłomiej Gasyna <bartlomiej.gasyna@student.put.poznan.pl>
2024-03-25 00:34:09 +0100

    TRT engine utilities

--- src/cones_detect_node.cpp
+++ src/cones_detect_node.cpp
@@ -14,6 +14,11 @@
 
 #include "cones_detect/cones_detect_node.hpp"
 
+cv::Rect get_rect(BBox box) {
+    return cv::Rect(round(box.x1), round(box.y1), round(box.x2 - box.x1), round(box.y2 - box.y1));
+}
+
+
 namespace cones_detect
 {
 
@@ -22,7 +27,52 @@ ConesDetectNode::ConesDetectNode(const rclcpp::NodeOptions & options)
 {
   cones_detect_ = std::make_unique<cones_detect::ConesDetect>();
   param_name_ = this->declare_parameter("param_name", 456);
-  cones_detect_->foo(param_name_);
+
+  build_engine = this->declare_parameter("build_engine", false);
+  onnx_path = this->declare_parameter("model_path", "model.onnx");
+  engine_path = this->declare_parameter("engine_path", "engine.engine");
+
+  std::cout << "onnx_path: " << onnx_path << std::endl;
+  std::cout << "engine_path: " << engine_path << std::endl;
+
+  setenv("CUDA_MODULE_LOADING", "LAZY", 1);
+
+  // rclcpp::init(argc, argv);
+  // auto node = std::make_shared<rclcpp::Node>("yolo_publisher");
+
+  // auto yellow_pub = node->create_publisher<geometry_msgs::msg::PoseArray>("yellow_cones", 10);
+  // auto blue_pub = node->create_publisher<geometry_msgs::msg::PoseArray>("blue_cones", 10);
+  // auto image_pub = node->create_publisher<sensor_msgs::msg::Image>("cones_img", 1);
+
+  // if (argc == 1) {
+  //       std::cout << "Usage: \n 1. ./yolo_onnx_zed -s yolov8s.onnx yolov8s.engine\n 2. ./yolo_onnx_zed -s yolov8s.onnx yolov8s.engine images:1x3x512x512\n 3. ./yolo_onnx_zed yolov8s.engine <SVO path>" << std::endl;
+  //       return 0;
+  //  }
+    
+
+    if (build_engine){
+  // Check Optim engine first
+  // if (std::string(argv[1]) == "-s" && (argc >= 4)) {
+      // std::string onnx_path = std::string(argv[2]);
+      // std::string engine_path = std::string(argv[3]);
+
+      OptimDim dyn_dim_profile;
+
+      // if (argc == 5) {
+      //     std::string optim_profile = std::string(argv[4]);
+      //     bool error = dyn_dim_profile.setFromString(optim_profile);
+      //     if (error) {
+      //         std::cerr << "Invalid dynamic dimension argument, expecting something like 'images:1x3x512x512'" << std::endl;
+      //         return EXIT_FAILURE;
+      //     }
+      // }
+
+      Yolo::build_engine(onnx_path, engine_path, dyn_dim_profile);
+      std::cout << "Build finished" << std::endl;
+
+  // }
+
+}
 }
 
 }  // namespace cones_detect
